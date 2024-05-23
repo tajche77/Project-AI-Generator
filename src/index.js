ed427ada74a9910b954f915cdbba59a2d6578113
tajche77 <bozic.tamara@gmail.com>
2024-05-23 15:35:54 +0200

    added basic js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generateRecipe(event) {
+  event.preventDefault();
 
+  new Typewriter("#recipe", {
+    strings: "The recipe will go here",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let recipeFormElement = document.querySelector("recipe-generator-form");
+recipeFormElement.addEventListener("submit", generateRecipe);
