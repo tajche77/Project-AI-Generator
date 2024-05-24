db3369cce51d20d4277812746dacb4add7f8a0f3
tajche77 <bozic.tamara@gmail.com>
2024-05-24 13:22:47 +0200

    removed console.log

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 function displayRecipe(response) {
-  console.log("recipe generated");
   new Typewriter("#recipe", {
     strings: response.data.answer,
     autoStart: true,
@@ -17,10 +16,6 @@ function generateRecipe(event) {
   let context =
     "You are a master pastry chef, expert in easy to make desserts. Your mission is to generate a short recipe in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Sign the recipe with 'SheCodes AI' inside a <strong> element at the end of the recipe and NOT at the beginning";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
-
-  console.log("generating a recipe");
-  console.log(`Prompt: ${promot}`);
-  console.log(`Contrext: ${context}`);
 
   let recipeElement = document.querySelector("#recipe");
   recipeElement.classList.remove("hidden");
